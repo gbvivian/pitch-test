faf4b8ce2410cc9ccfbb2024871a6ea18d556e6a
Vivian Lam <vivian.lam@shopify.com>
2020-06-03 19:09:49 -0400

    Refactor App.js to use testSequence

--- src/App.js
+++ src/App.js
@@ -1,30 +1,18 @@
 import React from 'react';
-import pianoA from '../src/assets/sounds/piano-a_A_major.wav';
-import pianoB from '../src/assets/sounds/piano-a_A_major.wav';
-import pianoC from '../src/assets/sounds/piano-a_A_major.wav';
-import pianoD from '../src/assets/sounds/piano-a_A_major.wav';
 
 import './App.css';
 
-import { Howl } from 'howler';
-
-function createHowl(song) {
-  return new Howl({
-    src: [song],
-  });
-}
+import { testSequence } from '../src/TestPlaylist.js';
 
 function App() {
-  const noteA = createHowl(pianoA);
-
   return (
     <div className="App">
       <button
         onClick={() => {
-          noteA.play();
+          testSequence.play();
         }}
       >
-        A
+        Play sequence
       </button>
     </div>
   );
