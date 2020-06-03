ee4d32632aa45393f2b1740453a1d273ac9b9dc6
Vivian Lam <vivian.lam@shopify.com>
2020-06-03 18:44:00 -0400

    Plays note A on button click

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
+import pianoA from '../src/assets/sounds/piano-a_A_major.wav';
 import './App.css';
 
+import { Howl } from 'howler';
+
 function App() {
+  var noteA = new Howl({
+    src: [pianoA],
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button
+        onClick={() => {
+          noteA.play();
+        }}
+      >
+        A
+      </button>
     </div>
   );
 }
