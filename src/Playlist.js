994166bf965d8b9a5ea8d75302c3a5df3141f872
Vivian Lam <vivian.lam@shopify.com>
2020-06-03 19:09:24 -0400

    Create playlist class and fix bug in onend()

---
+++ src/Playlist.js
@@ -0,0 +1,50 @@
+import { Howl } from 'howler';
+
+export default class Playlist {
+  songs = [];
+  currentSongIndex = 0;
+
+  constructor(listOfSongs) {
+    for (var song of listOfSongs) {
+      this.songs.push(
+        new Howl({
+          html5: true,
+          preload: false,
+          src: [song],
+          onend: () => {
+            if (this.currentSongIndex + 1 >= this.songs.length) {
+              this.currentSong.stop();
+            } else {
+              this.currentSongIndex++;
+              this.play();
+            }
+          },
+        })
+      );
+    }
+  }
+
+  get currentSong() {
+    return this.songs[this.currentSongIndex];
+  }
+
+  play() {
+    this.currentSong.play();
+  }
+
+  pause() {
+    this.currentSong.pause();
+  }
+
+  next() {
+    this.currentSong.stop();
+
+    if (this.currentSongIndex + 1 >= this.songs.length) {
+      this.currentSongIndex = 0;
+      this.play();
+    } else {
+      this.currentSongIndex++;
+      this.play();
+    }
+  }
+}
